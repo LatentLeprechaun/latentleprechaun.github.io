922d9becfc83ea54da718b513c01427acd688f39
Latent Leprechaun <latentleprechaun@gmail.com>
2016-07-29 18:20:34 -0600

    removed script thing and tweaked my name

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 init = function() {
-  document.getElementById('ditig').innerHTML = "Pay no mind to this (" + window.location.href + ")";
+  //document.getElementById('ditig').innerHTML = "Pay no mind to this (" + window.location.href + ")";
+  console.info('Good day sir! Javascript has been successfully initialized!');
 };
