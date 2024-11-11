27e3f8f8d3b8241442ca15c28fb7a13bd4ff91b1
LatentLeprechaun <latentleprechaun@gmail.com>
2024-11-10 17:26:29 -0700

    Moving some functions into site-wide section as utilities.

--- js/script.js
+++ js/script.js
@@ -128,44 +128,6 @@ init = function(pageLocID) {
       }
     };
 
-    // Function to make elements a little faster to create with less code. Any passthrough variable that is not needed should be declared as null.
-    // Example: let sideButton = makeSimpleButton("div", "simpleButton1", "buttonClass");
-    // Returns an element object
-    var makeSimpleElem = function(elementKind, idN, classN) {
-
-      let simpleElement = document.createElement(elementKind ? elementKind : "div");
-
-      simpleElement.id = idN ? idN : null;
-
-      simpleElement.className = classN ? classN : null;
-
-      return simpleElement;
-    };
-
-
-    //Function that takes a number, a value to add to the number, and a lower value and higher value to specify a range.
-    //Adds addVal to origVal as long is it doesn't exceed the range.
-    //Range inclusive. Returns an object with the sum of origVal+addVal and a boolean value specifying whether requested summation is within bounds.
-    //Example: aggregateWithinBounds(-4, 10, numberToAddTo, 1);
-    var aggregateWithinBounds = function(rangeOne, rangeTwo, origVal, addVal) {
-      //Throws error if rangeOne isn't less than rangeTwo.
-      if(rangeOne < rangeTwo){
-        if (origVal + addVal >= rangeOne && origVal + addVal <= rangeTwo){
-          console.log("Requested summation successful.");
-          return {resultNum:origVal+addVal, isWithinRange: true};
-        } else {
-          console.log("Requested summation using aggregateWithinBounds is out of specified range.");
-          return {resultNum:origVal, isWithinRange: false};
-        }
-      } else {
-        console.error("Function aggregateWithinBounds was likely given bad value(s). First range value should be less than the second range value. rangeOne: "+rangeOne+" rangeTwo: "+rangeTwo);
-        return;
-      }
-    };
-
-
-
-
 
     //Function that all images call to that pops up the gallery when an image is clicked
     var galleryPopup = function(imgNum) {
@@ -300,6 +262,47 @@ init = function(pageLocID) {
 
 
   //WRITE SITE-WIDE CODE BELOW THIS LINE
+
+
+
+
+  //UTILITY FUNCTIONS
+
+  // Function to make elements a little faster to create with less code. Any passthrough variable that is not needed should be declared as null.
+  // Example: let sideButton = makeSimpleButton("div", "simpleButton1", "buttonClass");
+  // Returns an element object
+  // TODO: look into function currying to make this better
+  var makeSimpleElem = function(elementKind, idN, classN) {
+
+    let simpleElement = document.createElement(elementKind ? elementKind : "div");
+
+    simpleElement.id = idN ? idN : null;
+
+    simpleElement.className = classN ? classN : null;
+
+    return simpleElement;
+  };
+
+  //Function that takes a number, a value to add to the number, and a lower value and higher value to specify a range.
+    //Adds addVal to origVal as long is it doesn't exceed the range.
+    //Range inclusive. Returns an object with the sum of origVal+addVal and a boolean value specifying whether requested summation is within bounds.
+    //Example: aggregateWithinBounds(-4, 10, numberToAddTo, 1);
+    var aggregateWithinBounds = function(rangeOne, rangeTwo, origVal, addVal) {
+      //Throws error if rangeOne isn't less than rangeTwo.
+      if(rangeOne < rangeTwo){
+        if (origVal + addVal >= rangeOne && origVal + addVal <= rangeTwo){
+          console.log("Requested summation successful.");
+          return {resultNum:origVal+addVal, isWithinRange: true};
+        } else {
+          console.log("Requested summation using aggregateWithinBounds is out of specified range.");
+          return {resultNum:origVal, isWithinRange: false};
+        }
+      } else {
+        console.error("Function aggregateWithinBounds was likely given bad value(s). First range value should be less than the second range value. rangeOne: "+rangeOne+" rangeTwo: "+rangeTwo);
+        return;
+      }
+    };
+
 
   switch(pageLocID){
     case "index":
